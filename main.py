81bc306b7e9e40c635805b8de3b35d470af37a71
Andrew <kicheevandrey@gmail.com>
2023-08-28 06:56:27 +0300

    Stage 2. * Changed the names of some variables and functions to more meaningful ones. * Added a more user-friendly interface.

--- main.py
+++ main.py
@@ -2,64 +2,79 @@ from random import shuffle
 
 
 def generate_dominoes() -> list:
-    duplicated_dominoes = []
+    """Return domino pool."""
+    duplicated_dominoes = []  # domino combinations with repetitions
     for i in range(7):
         for j in range(7):
             duplicated_dominoes.append(sorted([i, j]))
 
-    dominoes = []
+    dominoes = []  # clean domino pool (without repetitions)
     for d in duplicated_dominoes:
         if d not in dominoes:
             dominoes.append(d)
 
     return dominoes
 
-def finding_first_player(first_player: list, second_player) -> list:
-    fp_max = max(first_player)
-    sp_max = max(second_player)
+def finding_start_domino(player_one: list, player_two: list) -> list:
+    """Finding tht start player"""
 
-    if fp_max > sp_max:
-        return first_player.pop(first_player.index(fp_max))
-    elif fp_max < sp_max:
-        return second_player.pop(second_player.index(sp_max))
+    player_one_doubles = [d for d in player_one if d[0] == d[1]]
+    player_two_doubles = [d for d in player_two if d[0] == d[1]]
+
+    try:
+        player_one_max_double = max(player_one_doubles)
+    except ValueError:
+        player_one_max_double = [-1, -1]
+    try:
+        player_two_max_double = max(player_two_doubles)
+    except ValueError:
+        player_two_max_double = [-1, -1]
+
+    if player_one_max_double > player_two_max_double:
+        return player_one.pop(player_one.index(player_one_max_double))
     else:
-        return []
+        return player_two.pop(player_two.index(player_two_max_double))
 
-def start_deal(stock: list) -> (list, list, list, list):
-    duplicate_trigger = False
+def start_deal(dominoes: list) -> (list, list, list, list):
+    """Distribution of start hands.
+    Return: first player hand, second player hand, stock, start domino
+    """
+    player_double_exist = False
 
-    while not duplicate_trigger:
-        deal_stock = stock
-        shuffle(deal_stock)
-        first_player, second_player = [], []
+    while not player_double_exist:
+        shuffle(dominoes)
+        player_one, player_two = [], []
 
         for _ in range(7):
-            current_pop = deal_stock.pop()
+            current_pop = dominoes.pop()
             if current_pop[0] == current_pop[1]:
-                duplicate_trigger = True
-            first_player.append(current_pop)
+                player_double_exist = True
+            player_one.append(current_pop)
 
-            current_pop = deal_stock.pop()
+            current_pop = dominoes.pop()
             if current_pop[0] == current_pop[1]:
-                duplicate_trigger = True
-            second_player.append(current_pop)
+                player_double_exist = True
+            player_two.append(current_pop)
 
-        start_domino = finding_first_player(first_player, second_player)
-        if not start_domino:
-            duplicate_trigger = False
+        start_domino = finding_start_domino(player_one, player_two)
 
-    return first_player, second_player, deal_stock, start_domino
+    return player_one, player_two, dominoes, start_domino
 
 
 if __name__ == "__main__":
     computer_pieces, player_pieces, stock_pieces, domino_snake = start_deal(generate_dominoes())
-    if len(computer_pieces) > len(player_pieces):
-        start_player = 'computer'
-    else:
-        start_player = 'player'
 
-    print("Stock pieces:", stock_pieces)
-    print("Computer pieces:", computer_pieces)
-    print("Player pieces:", player_pieces)
-    print("Domino snake:", [domino_snake])
-    print("Status:", start_player)
+    print('='*70)
+    print('Stock size:', len(stock_pieces))
+    print('Computer pieces:', len(computer_pieces))
+    print()
+    print(domino_snake)
+    print()
+    print('Your pieces:')
+    for i, d in enumerate(player_pieces, start=1):
+        print(f'{i}:{d}')
+    print()
+    if len(computer_pieces) > len(player_pieces):
+        print("Status: Computer is about to make a move. Press Enter to continue...")
+    else:
+        print("Status: It's your turn to make a move. Enter your command.")
